7edaa0f495c45468081853e57176949baaddfdaf
Whisdangist <whisdangist@outlook.com>
2020-06-22 19:58:58 +0800

    加入预览功能

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <div id="app">
     <div class="input-box">
       <input type="file" id="image-input" @change="upload" accept="image/png,image/gif,image/jpeg">
+      <img id="input-preview">
     </div>
   </div>
 </template>
@@ -19,6 +20,7 @@ export default {
       let image = imageInput[0]
       let formData = new FormData()
       formData.append('dirty-image', image)
+      this.preview(image)
 
       let xhr = new XMLHttpRequest()
       xhr.open('POST', '/api/upload')
@@ -30,6 +32,14 @@ export default {
         }
       }
       xhr.send(formData)
+    },
+    preview: function (image) {
+      // 有的浏览器没有FileReader 后期可以加入兼容性代码
+      let reader = new FileReader()
+      reader.onload = event => {
+        document.getElementById('input-preview').src = event.target.result
+      }
+      reader.readAsDataURL(image)
     }
   }
 }
